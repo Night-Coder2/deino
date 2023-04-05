9e4d71c89e7e1bb5aa65c9debbdaa9b83bce5814
PhoenixLegen <>
2023-04-05 04:58:46 +0000

    ban

--- commands/ban.js
+++ commands/ban.js
@@ -2,13 +2,15 @@ module.exports = {
   name: 'ban',
   description: "This command bans a member!",
   execute(client, message, args, Discord) {
-    const target = message.mentions.users.first();
-    if (target) {
-      const memberTarget = message.guild.members.cache.get(target.id);
-      memberTarget.ban();
-      message.channel.send("User has been banned");
-    } else {
-      message.channel.send(`You coudn't ban that member!`);
+    if(message.member.permissions(Discord.PermissionsBitField.Flags.BanMembers || Discord.PermissionsBitField.Flags.Administrator)){
+      const target = message.mentions.users.first();
+      if (target) {
+          const memberTarget = message.guild.members.cache.get(target.id);
+          memberTarget.ban();
+          message.channel.send("User has been banned");
+      } else {
+        message.channel.send(`You coudn't ban that member!`);
+      }
     }
   }
 }
